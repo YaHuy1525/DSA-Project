ba64890cfb2362640bc5be605d93c4592fedb740
Yahuy1525 <huytrian2005@gmail.com>
2025-06-02 17:23:19 +0700

    Backend appointment system

---
+++ Backend/Models/AppointmentSystem.py
@@ -0,0 +1,35 @@
+from Models.AppointmentHistory import AppointmentHistory
+from Models.User import HashTable, User
+from Models.Appointment import AppointmentBST
+
+class AppointmentSystem:
+    def __init__(self):
+        self.users = HashTable()
+        self.appointments = AppointmentBST()
+    
+    def register_user(self, name: str, email: str, password: str):
+        existing_user = self.users.get(email)
+        if existing_user:
+            return False, "User already exists"
+        user = User(name, email, password)
+        self.users.insert(email, user)
+        self.users.display()
+        return True, "User registered successfully"
+    
+    def authenticate_user(self, email: str, password: str):
+        user = self.users.get(email)
+        if not user:
+            return False, "User not found", None
+        if user.password != password:
+            return False, "Invalid password", None
+        return True, "Login successful", user.name
+    
+    def get_user_appointments(self, username):
+        self.appointments.display_appointments()
+        return self.appointments.get_user_appointments(username)
+    
+    def book_appointment(self, date, time, username):
+        return self.appointments.insert(date, time, username)
+    
+    def remove_appointment(self, username):
+        return self.appointments.delete_appointment(username)
